990709904ccd4c162343291f5163cb9e1f79952f
Alejandro Eduardo Cruz Paz <aecp@ciencias.unam.mx>
2011-03-02 17:16:43 -0600

    Clase encargada de hacer activaciones y desactivaciones informando a los usuarios por email desactivada

--- app/mailers/notificador.rb
+++ app/mailers/notificador.rb
@@ -2,22 +2,4 @@
 
 class Notificador < ActionMailer::Base
   default :from => "clusterciudadano@gmail.com"
-
-  def desactivacion(usuario)
-    notificacion_comun(usuario, 'Tú cuenta en animalitos ha sido desactivada. :(')
-  end
-  
-  def activacion(usuario)
-    notificacion_comun(usuario, 'Tú cuenta en animalitos ha sido activada! :)')
-  end
-  
-  def reset_password_instruccions(usuario)
-    notificacion_comun(usuario, 'Instrucciones para restaurar tú contraseña')
-  end
-  
-  def notificacion_comun(usuario, msj)
-    mail(:to => usuario.email,
-         :subject => msj)
-    end
-  end
 end
