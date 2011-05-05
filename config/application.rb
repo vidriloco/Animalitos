8c269cbb0eb6ff051d40b018cfe5c9c72d2ffa26
Alejandro Eduardo Cruz Paz <aecp@ciencias.unam.mx>
2011-05-05 01:44:21 -0500

    Cambios menores relacionados con la fecha-tiempo del registro de un animalito. Establecida una zona horaria también

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,8 @@ module Froyito
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
-
+    config.time_zone = 'Central Time (US & Canada)'  
+    
     config.action_view.javascript_expansions[:defaults] = %w(jquery.min jquery.cycle.all.min rails application)
     config.action_view.javascript_expansions[:jquery_exts] = %w(jquery.form jquery.remotipart jquery.charcounter jquery.ba-bbq.min jquery.cookie)
     config.action_view.javascript_expansions[:others] = %w(slimbox2)
